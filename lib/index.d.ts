0627c3804e4d94e5fde44da7313286d85478fb54
huangdonghao <huangdonghao@miya818.com>
2022-01-25 13:57:45 +0800

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,7 +3,13 @@ import Wind from "./script/physics/Wind";
 import Borders from "./script/physics/Borders";
 import EleBorder from "./script/plugin/EleBorder";
 declare const _default: {
+    /**
+     * 雪花动画类
+     */
     Snow: typeof Snow;
+    /**
+     * 功能扩展模块
+     */
     Mod: {
         Wind: typeof Wind;
         Borders: typeof Borders;
